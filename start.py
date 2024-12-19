abafc360643aec899cf1505fd8f747ed24abcccf
Samuel Ostholm <sostholm@gmail.com>
2024-12-19 16:00:30 +0100

    fixing package structure

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import uvicorn
-from assistant_process_audio import app
+from assistant_process_audio.server import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9000)
