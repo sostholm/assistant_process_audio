880adcd1388ea7e57c152709501cebac65f4da56
Samuel Ostholm <sostholm@gmail.com>
2025-03-20 21:30:59 +0100

    fixing voice data query

--- assistant_process_audio/database.py
+++ assistant_process_audio/database.py
@@ -3,21 +3,23 @@ import os
 from dataclasses import dataclass
 from typing import List, Optional
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_USER     = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE", "assistant_testing")
+POSTGRES_HOST     = os.getenv("POSTGRES_HOST", "192.168.0.218")
+POSTGRES_PORT     = os.getenv("POSTGRES_PORT", "5432")
 # Connect to your PostgreSQL database
 conn = psycopg.connect(
-    dbname="assistant_v3",
+    dbname=POSTGRES_DATABASE,
     user=POSTGRES_USER,
     password=POSTGRES_PASSWORD,
-    host="192.168.0.218",
-    port="5432"
+    host=POSTGRES_HOST,
+    port=POSTGRES_PORT
 )
 
 @dataclass
 class UserVoiceRecognition:
     user_id: str
-    name: str
     nick_name: Optional[str]
     voice_recognition: List[bytes]
 
@@ -26,24 +28,31 @@ def get_users_voice_recognition() -> List[UserVoiceRecognition]:
     cursor.execute("""
         SELECT
             u.user_id,
-            COALESCE(up.full_name, 'Unknown') AS name,
-            COALESCE(up.nick_name, 'Unknown') AS nick_name,
-            array_agg(vr.voice_recognition) AS voice_data
+            u.nick_name AS nick_name,
+            vr.voice_recognition AS voice_data
         FROM users u
-        LEFT JOIN user_profile up ON u.user_profile_id = up.user_profile_id
-        LEFT JOIN voice_recognition vr ON u.user_id = vr.user_id
-        GROUP BY u.user_id, up.full_name, up.nick_name;
+        LEFT JOIN voice_recognition vr ON u.user_id = vr.user_id;
     """)
     rows = cursor.fetchall()
 
+    cursor.execute("""
+        SELECT
+            ai.ai_id,
+            ai.ai_name,
+            vr.voice_recognition AS voice_data
+        FROM ai 
+        LEFT JOIN voice_recognition vr ON ai.ai_id = vr.ai_id;""")
+    
+    rows += cursor.fetchall()
+    cursor.close()
+
     results = []
     for row in rows:
-        user_id, name, nick_name, voice_data = row
+        user_id, nick_name, voice_data = row
         if voice_data is None:
-            voice_data = []
+            continue
         results.append(UserVoiceRecognition(
             user_id=user_id,
-            name=name,
             nick_name=nick_name,
             voice_recognition=voice_data
         ))
